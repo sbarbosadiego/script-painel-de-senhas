a233268e099fe0e670cfd1375344e03e88e7a74e
Diego Barbosa <diego.barbosa.pro@gmail.com>
2022-11-03 09:44:33 -0300

    Update painel.sh
    - Corrigido links para download os arquivos css;
    - Adicionado a linha de acesso ao diretório aonde é configurado os arquivos.

--- painel.sh
+++ painel.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-
+cd /home/alpha7
 mkdir chinchila-online-arquivos && cd chinchila-online-arquivos
 wget -q https://cdn.discordapp.com/attachments/981227657595330623/1029111840929746974/logo_a7.png
-echo -n "===================================================
+echo -n "=================================================================
 (1)Padrão
 (2)Maxi Popular
 (3)Ultra Popular
@@ -13,39 +13,42 @@ read css
 case $css in
 	1)
 		echo "Baixado Painel Padrão"
-		wget -q https://github.com/DiegoBarbosa-S/script-painel-de-senhas/blob/6d0144db79086cb9f6b9172fabc13fbe1cde1108/css/padr%C3%A3o/atendimento.css
+		wget -q https://raw.githubusercontent.com/BillyaD/script-painel-de-senhas/main/css/padr%C3%A3o/atendimento.css
 		;;
 	2)
 		echo "Baixado Painel Maxi Popular"
-		wget -q https://github.com/DiegoBarbosa-S/script-painel-de-senhas/blob/6d0144db79086cb9f6b9172fabc13fbe1cde1108/css/maxi-popular/atendimento.css
+		wget -q https://raw.githubusercontent.com/BillyaD/script-painel-de-senhas/main/css/maxi-popular/atendimento.css
 		;;
 	3)
 		echo "Baixado Painel Ultra Popular"
-		wget -q https://github.com/DiegoBarbosa-S/script-painel-de-senhas/blob/6d0144db79086cb9f6b9172fabc13fbe1cde1108/css/ultra-popular/atendimento.css
+		wget -q https://raw.githubusercontent.com/BillyaD/script-painel-de-senhas/main/css/ultra-popular/atendimento.css
 		;;
 	4)
 		echo "Baixado Painel Escuro"
-		wget -q https://github.com/DiegoBarbosa-S/script-painel-de-senhas/blob/6d0144db79086cb9f6b9172fabc13fbe1cde1108/css/escuro/atendimento.css
+		wget -q https://raw.githubusercontent.com/BillyaD/script-painel-de-senhas/main/css/escuro/atendimento.css
 		;;	
 esac
 cd -
 touch imprimeEtiquetaAtendimento
 chmod u+x imprimeEtiquetaAtendimento
-echo "Dado Permissão de Execução ao Arquivo: imprimeEtiquetaAtendimento"
-echo -n "===================================================
+echo "=================================================================
+Dado Permissão de Execução ao Arquivo: imprimeEtiquetaAtendimento"
+echo -n "=================================================================
 (1)Tablet
 (2)Computador
 Será configurado: "
 read var
 case $var in
 	1)
-		echo "Configurando para Tablet"
+		echo "================================================================="
+		echo "                    Configurando para Tablet"
 		echo -n "Digite o IP do Tablet: "
 		read IP_DO_TABLET
 		echo "cat "\"\${1}\"" | nc -w 5 $IP_DO_TABLET 3333" >> imprimeEtiquetaAtendimento
 		;;
 	2)
-		echo "Configurando para Computador"
+		echo "================================================================="
+		echo "                   Configurando para Computador"
 		echo -n "Digite o IP do Computador: "
 		read IP_COMPUTADOR_INSTALACAO
 		echo -n "Digite o Nome de Compartilhamento da Impressora: "
@@ -58,7 +61,7 @@ case $var in
 		echo "smbclient //$IP_COMPUTADOR_INSTALACAO/$NOME_COMPARTILHAMENTO_IMPRESSORA -U "\"$USUARIO%$SENHA\"" -c "\"print "\${1}\"" >> imprimeEtiquetaAtendimento
 		;;
 esac
-echo -n "===================================================
+echo -n "=================================================================
 (1) Sim
 (2) Não
 Desfazer configuração do Painel de Senhas? "
